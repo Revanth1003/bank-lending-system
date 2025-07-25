c4e13111958a8219daf921cd0734e39379fd2fd6
Revanth <venkatrevanth2210@gmail.com>
2025-07-26 00:06:58 +0530

    Fix: Set host to 0.0.0.0 and bind port for Render

--- app.py
+++ app.py
@@ -139,4 +139,8 @@ def overview(loan_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
